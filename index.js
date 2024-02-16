9f5969dcd6da5367dd8bc74914f76548b1b158bf
Sataphat Denduang <phatkung.kizz@gmail.com>
2024-02-16 16:30:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,17 +17,14 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3000;
 //app.use(cors());
-
-app.use((req, res, next) => {
-  const allowedOrigins = ["*"]; // Add your allowed origins
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  // ... other CORS headers and configurations
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204, // For preflight requests
+  })
+);
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
